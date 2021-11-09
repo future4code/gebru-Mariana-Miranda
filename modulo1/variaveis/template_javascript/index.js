4b479f87ff8ee4790e9d2e2a48ffd0e0b7ea43b3
marianaacioly <93156474+marianaacioly@users.noreply.github.com>
2021-11-09 20:47:40 -0300

    adicionado index.html e index.js

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,73 @@
+/* EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO
+1. 
+10
+10,5
+
+2.
+10,10,10
+
+3. 
+let p = horasTrabalhadasPorDia
+let t = remuneracaoPordia
+*/
+
+let nome
+let idade
+console.log(typeof nome)
+console.log(typeof idade)
+
+
+/* c.
+undefined - Pois não foi atribuído um valor para a variável
+*/
+
+nome = "Mariana"
+idade = 36
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+/* e.
+Impresso string e number pois foi dado um valor à variável.
+*/
+
+console.log("Olá, meu nome é" , nome, "você tem" , idade, "anos." )
+
+2. 
+let vocebebeuAguaHoje
+let voceEstaAprendendoComOcurso
+let voceSeArrependeudeComeçasOcurso
+
+vocebebeuAguaHoje = true
+voceEstaAprendendoComOcurso = true
+voceSeArrependeudeComeçasOcurso = false
+
+console.log(voceEstaAprendendoComOcurso)
+console.log(vocebebeuAguaHoje)
+console.log(voceSeArrependeudeComeçasOcurso)
+
+3.
+let a =10
+let b = 25
+
+console.log ("o novo valor de a é", b)
+console.log ("O novo valor de b é", a)
+
+let c = 10
+a = b
+b = c
+console.log(a)
+console.log(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
